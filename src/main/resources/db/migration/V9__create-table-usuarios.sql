a6726bb4edc7690ac91c69ecb5c8af7d4514ac80
Artur Coutinho <arturpc@gmail.com>
2021-11-01 23:37:50 -0300

    Criadas as classes Usuario e Perfil. Criadas as migrations relacionadas a essas classes. Incluídas Spring Security e jjwt.

---
+++ src/main/resources/db/migration/V9__create-table-usuarios.sql
@@ -0,0 +1,7 @@
+CREATE TABLE `usuarios` (
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `nome` varchar(255) NOT NULL,
+  `login` varchar(255) NOT NULL,
+  `senha` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
+);
